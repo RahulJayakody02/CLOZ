642a03b1ff67c8791b2cdd5e2bdadfd777f72cab
Rahul Jayakody <rahuljayakody07@gmail.com>
2025-03-10 00:18:27 +0530

    inventory CRUD and filtering part done

---
+++ backend/models/product_model.js
@@ -0,0 +1,82 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+  productId: { 
+    type: String, 
+    unique: true,
+    required: true },
+
+  name: { 
+    type: String, 
+    required: true },
+
+  description: { 
+    type: String },
+
+  category: {
+     type: String, 
+     required: true },
+
+  brand: { 
+    type: String },
+
+  size: 
+  { type: String  },
+
+  color: { 
+    type: [String],
+     default: [] },
+
+  material: {
+    type: String },
+
+  gender: { 
+    type: String, 
+    enum: ['Men', 'Women', 'Unisex'], 
+    required: true },
+
+  quantityInStock: { 
+    type: Number, 
+    required: true, 
+    min: 0 },
+
+  lowStockThreshold: { 
+    type: Number, 
+    default: 5 },
+
+  supplier: { 
+    type: String },
+
+  price: { 
+    type: Number, 
+    required: true },
+
+  discountPrice: { 
+    type: Number, 
+    default: 0 },
+
+
+  images: { 
+    type: [String], 
+    default: [] },
+
+  status: { 
+    type:String},
+
+  addedDate: { 
+    type: Date, 
+    default: Date.now },
+
+  updatedAt: { 
+    type: Date,
+    default: Date.now }
+});
+
+// Middleware to update `updatedAt` before saving
+//productSchema.pre('save', function (next) {
+  //this.updatedAt = Date.now();
+  //next();
+//});
+
+const Product = mongoose.model('Product', productSchema);
+export default Product;
